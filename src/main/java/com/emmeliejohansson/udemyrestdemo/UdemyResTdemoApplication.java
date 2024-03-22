c523cbc768e763900b24f3426dade69985b3afe2
Emmelie83 <info@emmeliejohansson.com>
2024-03-22 23:57:42 +0100

    Initial commit

---
+++ src/main/java/com/emmeliejohansson/udemyrestdemo/UdemyResTdemoApplication.java
@@ -0,0 +1,13 @@
+package com.emmeliejohansson.udemyrestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyResTdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyResTdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emmeliejohansson/udemyrestdemo/UdemyResTdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emmeliejohansson.udemyrestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyResTdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
